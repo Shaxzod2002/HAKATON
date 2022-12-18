e4ef2fa71cbe6ad257e1915b664c5bebbe21d1e3
Raximov SHaxzodbek <raximovshaxzodbek2002@gmail.com>
2022-12-19 00:09:24 +0500

    new fitness and clonthing page

---
+++ src/pages/WomensRights.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const WomensRights = () => {
+  return (
+    <div>
+      <Navbar />
+      <header>
+        <h1>Womens Rights</h1>
+      </header>
+      <Footer />
+    </div>
+  );
+};
+
+export default WomensRights;
